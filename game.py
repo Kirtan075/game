484ebee51d0186e0cbd19b475291be127aaf26db
Kirtan075 <kirtanp046@gmail.com>
2024-11-08 20:59:29 -0500

    1st and 2nd

---
+++ game.py
@@ -0,0 +1,7 @@
+def main():
+    print("Welcome to Rock, Paper, Scissors Game!")
+    print("Instructions: Choose 'rock', 'paper', or 'scissors'.")
+    print("The computer will also make a choice. See who wins!")
+
+if _name_ == "_main_":
+    main()
